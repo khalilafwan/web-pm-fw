6201947815be4e8669f6af1210e33d7755c907bd
Khalilul Afwan <khalilulafwan@student.unp.ac.id>
2024-06-24 10:38:43 +0700

    fix migrations

--- database/migrations/2024_06_21_095148_create_data_monitorings_table.php
+++ database/migrations/2024_06_21_095148_create_data_monitorings_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('DataMonitoring', function (Blueprint $table) {
+        Schema::create('data_monitoring', function (Blueprint $table) {
             $table->id();
             $table->string('no_jo')->nullable();
             $table->date('tgl_jo')->nullable();
--- database/migrations/2024_06_21_095154_create_data_konsesis_table.php
+++ database/migrations/2024_06_21_095154_create_data_konsesis_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('DataKonsesi', function (Blueprint $table) {
+        Schema::create('data_konsesi', function (Blueprint $table) {
             $table->id();
             $table->string('jo')->nullable();
             $table->string('wo')->nullable();
